ac082b71c049ac1cfa28adfdcffa2069075bc9f9
Paulo Santos <pauloagsantos@gmail.com>
2025-10-03 15:42:05 +0100

    Update Ficha2.java

--- Ficha2/src/ficha2/Ficha2.java
+++ Ficha2/src/ficha2/Ficha2.java
@@ -4,6 +4,7 @@
  */
 package ficha2;
 
+
 /**
  *
  * @author IPT
@@ -14,7 +15,38 @@ public class Ficha2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("LimitedStack  :" + runXtimes(new LimitedStack(1000000),10));
+        System.out.println("IlimitedStack :" + runXtimes(new IlimitedStack(),10));
+        System.out.println("Java Stack    :" + runXtimes(new java.util.Stack(),10));
+    
+    }
+    
+    private static long runXtimes(Stack s, int n) {
+        long sum=0;
+        for(int i=0; i < n; i++) {
+            long ini = System.nanoTime();
+            for(int j=0; j<1000000; j++)
+                s.push(Math.random());
+            while(!s.isEmpty())
+                s.pop();
+            long fim = System.nanoTime();
+            sum += fim-ini;
+        }
+        return sum/n;
+    }
+    
+    private static long runXtimes(java.util.Stack s, int n) {
+        long sum=0;
+        for(int i=0; i < n; i++) {
+            long ini = System.nanoTime();
+            for(int j=0; j<1000000; j++)
+                s.push(Math.random());
+            while(!s.isEmpty())
+                s.pop();
+            long fim = System.nanoTime();
+            sum += fim-ini;
+        }
+        return sum/n;
     }
     
 }
