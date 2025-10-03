883c487cc94ea6dda8d3d577a89b993ac3959481
Paulo Santos <pauloagsantos@gmail.com>
2025-10-03 15:00:37 +0100

    LimitedStack

---
+++ Ficha2/src/ficha2/LimitedStack.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ficha2;
+
+/**
+ *
+ * @author IPT
+ */
+public class LimitedStack implements Stack {
+    
+    private final Object array[];
+    private int top;
+    
+    public LimitedStack(int maxSize) {
+        array = new Object[maxSize];
+        top = -1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    @Override
+    public void push(Object item) {
+        top++;
+        array[top] = item;
+    }
+
+    @Override
+    public Object pop() {
+        if (isEmpty())
+            return null;
+        return array[top--];
+    }
+
+    @Override
+    public Object peek() {
+        if (isEmpty())
+            return null;
+        return array[top];    
+    }
+    
+}
